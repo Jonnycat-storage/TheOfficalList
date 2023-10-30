017583093919fb54d8d4bea77b6d46cf288f02c7
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2023-10-29 22:05:38 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,117 @@
 document.getElementById("search-button").addEventListener("click", function () {
     const searchValue = document.getElementById("search-input").value;
+    const resultsContainer = document.getElementById("result");
 
-    // Fetch the links from the text file
-    fetch("lightspeed.txt") //gets and reads the links inside of the text file
-        .then((response) => response.text())
-        .then((data) => {
-            const links = data.split("\n");
+    while (resultsContainer.firstChild) {
 
-            // Search for the link corresponding to the search value
-            for (const link of links) {
-                const [name, url] = link.split(" ");
-                if (name.toLowerCase() === searchValue.toLowerCase()) {
-                    // Open the link in a new tab without adding to browser history
-                    window.open(url, "_blank", "noopener,noreferrer");
-                    return;
-                }
-            }
 
-            // Display a message if no matching link is found
-            document.getElementById("results").textContent = "No matching link found.";
-        })
-        .catch((error) => {
-            console.error("Error fetching links:", error);
-        });
+        resultsContainer.removeChild(resultsContainer.firstChild);
+
+    }
+    var breake = document.createElement('br')
+    var breake2 = document.createElement('br')
+    var breake3 = document.createElement('br')
+    var breake4 = document.createElement('br')
+
+    
+    resultsContainer.appendChild(breake)
+    resultsContainer.appendChild(breake2)
+    resultsContainer.appendChild(breake3)
+    resultsContainer.appendChild(breake4)
+
+
+    fetch("lightspeed.txt")
+  .then((response) => response.text())
+  .then((data) => {
+    const links = data.split("\n");
+    const resultsContainer = document.getElementById("result");
+
+    for (const link of links) {
+      const [name, url] = link.split(" ");
+      if (name.toLowerCase().includes(searchValue.toLowerCase())) {
+
+        const linkElement = document.createElement("a");
+        linkElement.href = url;
+        linkElement.target = "_blank";
+        linkElement.rel = "noopener,noreferrer";
+
+        const listItem = document.createElement("div");
+        listItem.classList.add('ITEMCLASS')
+      
+        const textstuff = document.createElement('h1')
+        textstuff.textContent = name;
+        textstuff.classList.add('fixedtxt')
+
+
+
+
+        listItem.appendChild(textstuff)
+
+
+
+       
+
+
+        linkElement.appendChild(listItem);
+
+        resultsContainer.appendChild(linkElement);
+      }
+    }
+
+    if (resultsContainer.children.length === 0) {
+      resultsContainer.textContent = "No matching links found.";
+    }
+  })
+  .catch((error) => {
+    console.error("Error fetching links:", error);
+  });
+
 });
+
+const searchValue = document.getElementById("search-input").value;
+
+    fetch("lightspeed.txt")
+  .then((response) => response.text())
+  .then((data) => {
+    const links = data.split("\n");
+    const resultsContainer = document.getElementById("result");
+
+    for (const link of links) {
+      const [name, url] = link.split(" ");
+      if (name.toLowerCase().includes(searchValue.toLowerCase())) {
+
+        const linkElement = document.createElement("a");
+        linkElement.href = url;
+        linkElement.target = "_blank";
+        linkElement.rel = "noopener,noreferrer";
+
+        const listItem = document.createElement("div");
+        listItem.classList.add('ITEMCLASS')
+      
+        const textstuff = document.createElement('h1')
+        textstuff.textContent = name;
+        textstuff.classList.add('fixedtxt')
+
+
+
+
+        listItem.appendChild(textstuff)
+
+
+
+       
+
+
+        linkElement.appendChild(listItem);
+
+        resultsContainer.appendChild(linkElement);
+      }
+    }
+
+    if (resultsContainer.children.length === 0) {
+      resultsContainer.textContent = "No matching links found.";
+    }
+  })
+  .catch((error) => {
+    console.error("Error fetching links:", error);
+  });
